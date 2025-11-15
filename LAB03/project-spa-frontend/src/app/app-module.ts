037547aab73d31ea0fa44c1a64723770c12337cb
justin <justin.noname@gmail.com>
2025-11-14 22:02:31 -0500

    part 3 done

--- LAB03/project-spa-frontend/src/app/app-module.ts
+++ LAB03/project-spa-frontend/src/app/app-module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { ProjectComponent } from './project/project.component';
+import{HttpClientModule} from '@angular/common/http';
+import {ProjectService} from './services/project.service';
 
 @NgModule({
   declarations: [
@@ -11,9 +13,11 @@ import { ProjectComponent } from './project/project.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
+    ProjectService,
     provideBrowserGlobalErrorListeners()
   ],
   bootstrap: [ProjectComponent]
